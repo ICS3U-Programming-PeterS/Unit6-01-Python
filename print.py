b67c4185aeaa3bf1413e59079403d6733d8257c4
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2022-12-18 00:56:41 +0000

    Added print.py

--- print.py
+++ print.py
@@ -20,15 +20,9 @@ def main():
 
     # displays random numbers and calculates average
     for counter in range(constants.MAX_ARRAY_SIZE):
-        list_of_ints.append(random.randint(constants.MIN_NUM,
-                                           constants.MAX_NUM))
+        list_of_ints.append(random.randint(0, 100))
         sum = sum + list_of_ints[counter]
-        if list_of_ints[counter] < 10:
-            print(f"{list_of_ints[counter]}  added to the" +
-                  " list at position {counter}")
-        else:
-            print(f"{list_of_ints[counter]} added to the" +
-                  " list at position {counter}")
+        print(f"{list_of_ints[counter]} added to list, at spot {counter}")
 
     # calculate and display average
     average = sum / constants.MAX_ARRAY_SIZE
